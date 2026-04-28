d035e792172cb8dd434adb854f5e417c933018aa
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-04-28 20:57:23 +0100

    feat(material): add Material model import and create new init file for material module

--- app/api/models/__init__.py
+++ app/api/models/__init__.py
@@ -1,3 +1,4 @@
 from app.api.models.user import User  # noqa: F401
 from app.api.models.announcement import Announcement, Signatory, announcement_signatories # noqa: F401
 from app.api.models.event import Event  # noqa: F401
+from app.api.models.material import Material, Department  # noqa: F401  
