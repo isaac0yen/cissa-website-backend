aa3c5915b412ce603f5998ddc2b670d616534101
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-04-28 20:22:49 +0100

    feat(material): add schemas for Material requests and responses

---
+++ app/api/v1/materials/schemas.py
@@ -0,0 +1,57 @@
+from datetime import date, datetime, time
+from typing import Annotated, Literal, Optional
+
+from pydantic import BaseModel, StringConstraints
+
+from app.core.base.schema import BaseResponseModel, PaginatedResponse
+
+LevelType = Literal["100", "200", "300", "400"]
+SemesterType = Literal["harmattan", "rain"]
+
+class MaterialRequest(BaseModel):
+    title: str
+    description: str
+    course_code: str
+    course_title: str
+    level: LevelType
+    semester: SemesterType
+    material_type: str
+    drive_url: str
+    session: str
+    departments: list[str]
+
+class MaterialUpdateRequest(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    course_code: Optional[str] = None
+    course_title: Optional[str] = None
+    level: Optional[LevelType] = None
+    semester: Optional[SemesterType] = None
+    material_type: Optional[str] = None
+    drive_url: Optional[str] = None
+    session: Optional[str] = None
+    departments: Optional[list[str]] = None
+
+class MaterialResponseData(BaseModel):
+    id: str
+    title: str
+    description: Optional[str]
+    course_code: str
+    course_title: str
+    level: LevelType
+    semester: SemesterType
+    material_type: str
+    drive_url: str
+    session: Optional[str]
+    departments: list[str]
+    created_at: datetime
+    updated_at: datetime
+
+class MaterialResponseModel(BaseResponseModel):
+    data: MaterialResponseData
+
+class MaterialPaginatedResponse(PaginatedResponse):
+    items: list[MaterialResponseData]
+
+class MaterialsListResponseModel(BaseResponseModel):
+    data: MaterialPaginatedResponse | PaginatedResponse
